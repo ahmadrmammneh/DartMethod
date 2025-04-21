3d00d42e0e67dd6ae462afc7646b3816a6fefeab
ahmadrmammneh <ahmad.mohammad18977@gmail.com>
2025-04-21 15:20:59 +0300

    update

---
+++ main.dart
@@ -0,0 +1,22 @@
+void main() {
+  String myname = "ahmad";
+
+  int myage = 27;
+
+  double myhighest = -190.4;
+
+  int myyear = 1998;
+
+  print(myname[0].toUpperCase());
+
+  print(myhighest.floor());
+  print(myhighest.ceil());
+  print(myhighest.round());
+  print(myhighest.isNegative);
+
+  print(myhighest.abs());
+  print("I was born in $myyear");
+
+  int myageAfter10YEar = myage + 10;
+  print("my age after 10 years is $myageAfter10YEar");
+}
